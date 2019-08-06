f1e2ad15ea1043a6a41f5b5d7711c9ee3d982670
timothy egart <timothyegart@Justins-MBP-3.localdomain>
2019-08-06 16:51:57 -0700

    completed the giphy lab

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Let's get this party started!");
+async function getGif(e) {
+  e.preventDefault();
+  let getSearchTerm = $(".enter").val();
+
+  let gifList = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: { api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", q: getSearchTerm }
+  });
+  selectGif(gifList);
+}
+
+function selectGif(gifs) {
+  let index = Math.floor(Math.random() * gifs.data.data.length);
+  let selectedGif = gifs.data.data[index].images.fixed_height.url;
+  $(".gallery").append(`<img src="${selectedGif}">`);
+}
+
+function removeGif() {
+  $("img").remove();
+}
+
+$(".search").on("click", getGif);
+$(".remove").on("click", removeGif);
